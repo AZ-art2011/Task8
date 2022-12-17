b960c22422e1a7ab45e0984afd865ff0396aabc9
Alexander <mypost@example.com>
2022-12-17 16:08:17 +0300

    продолжение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывод четных чисел в указанном диапазоне
+
+Console.WriteLine("Введите число - границу диапазона");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+for (int t=1; t < limit; t++){
+    if t % 2 == 0{
+        Console.WriteLine(t)
+    }
+}
